772231137d08cbb5f4f591f7d3d0d29f8930daaa
Briose <69117706+Briose@users.noreply.github.com>
2020-10-29 13:16:38 +0000

    Linux 3.4.70

--- goxgui/info.py
+++ goxgui/info.py
@@ -1,3 +1,7 @@
 from PyQt4.QtCore import QAbstractTableModel, QVariant, Qt, SIGNAL, pyqtSignal
 from PyQt4.QtGui import QColor
-from prefere
+from preferences import Preferences
+import money
+
+
+class Info(QAbstractTableMo
