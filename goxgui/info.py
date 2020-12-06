c2ebfe24c9545b0b33d49a9c7ba28430b5c7dab2
Briose <69117706+Briose@users.noreply.github.com>
2020-12-06 21:16:29 +0000

    Added score

--- goxgui/info.py
+++ goxgui/info.py
@@ -71,4 +71,7 @@ class Info(QAbstractTableModel):
 
         if row == 0 and (col == 0 or col == 1):
             self.signal_base_balance_clicked.emit()
-       
+            return
+
+        if row == 1 and (col == 0 or col == 1):
+            
