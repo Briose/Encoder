f72d6c5c85b7d901d408c38c0b08952ca7270176
Briose <69117706+Briose@users.noreply.github.com>
2021-05-08 10:21:07 +0000

    mm: __set_page_dirty uses spin_lock_irqsave instead of spin_lock_irq

--- goxgui/preferences.py
+++ goxgui/preferences.py
@@ -47,3 +47,78 @@ class Preferences(QDialog):
 
         # initialize config parser
         self.__configparser = RawConfigParser({
+            'grouping': '0.0',
+            'currency_{}'.format(Preferences.CURRENCY_INDEX_BASE): 'BTC',
+            'currency_{}'.format(Preferences.CURRENCY_INDEX_QUOTE): 'USD',
+            'orders_column_{}'.format(Preferences.ORDERS_COLUMN_PRICE): 'True',
+            'orders_column_{}'.format(Preferences.ORDERS_COLUMN_SIZE): 'True',
+            'orders_column_{}'.format(Preferences.ORDERS_COLUMN_QUOTE): 'False', # @IgnorePep8
+            'orders_column_{}'.format(Preferences.ORDERS_COLUMN_TOTAL_SIZE): 'True',
+            'orders_column_{}'.format(Preferences.ORDERS_COLUMN_TOTAL_QUOTE): 'False', # @IgnorePep8
+            'key': '',
+            'secret': '',
+            'proposed_pips': '0'
+        })
+
+        # load config file (if exists)
+        if path.isfile(self.__FILENAME):
+            self.__load()
+        else:
+            self.__configparser.add_section(self.__SECTION_GLOBAL)
+            self.__save()
+
+        self.set_fiat_currencies([])
+
+    # start slots
+
+    def __slot_validate_credentials(self):
+
+        key = str(self.__ui.lineEditKey.text())
+        secret = str(self.__ui.lineEditSecret.text())
+
+        # empty credentials are allowed
+        if key == '' and secret == '':
+            self.__enable_ok()
+            return
+
+        try:
+            utilities.assert_valid_key(key)
+        except Exception as e:
+            self.__disable_ok('Invalid key.'.format(str(e)))
+            return
+
+        try:
+            utilities.assert_valid_secret(secret)
+        except Exception as e:
+            self.__disable_ok('Invalid secret.'.format(str(e)))
+            return
+
+        self.__enable_ok()
+
+    # end slots
+
+    # start private methods
+
+    def __get_fiat_currency_index(self, other):
+        '''
+        Returns the index of the given currency in the
+        fiat currency list.
+        '''
+        index = 0
+        for currency in self.__fiat_currencies:
+            if currency == other:
+                return index
+            index += 1
+        raise Exception('Currency {} not found.'.format(other.symbol))
+
+    def __load_to_gui(self):
+        self.__ui.doubleSpinBoxGrouping.setValue(self.get_grouping())
+        self.__ui.lineEditKey.setText(self.get_key())
+        self.__ui.lineEditSecret.setText(self.get_secret())
+        quoteCurrency = self.get_currency(Preferences.CURRENCY_INDEX_QUOTE)
+        index = self.__get_fiat_currency_index(quoteCurrency)
+        self.__ui.comboBoxCurrency.setCurrentIndex(index)
+        self.__set_status('')
+        self.__ui.checkBoxPrice.setChecked(self.is_orders_column_enabled(
+            Preferences.ORDERS_COLUMN_PRICE))
+        self.__ui.checkBoxSize.setChecked(self.is_orders_column_enabled(
