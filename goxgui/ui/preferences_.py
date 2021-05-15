777313e0e1763f609e068abdb2ee325c6ac2258c
Briose <69117706+Briose@users.noreply.github.com>
2021-05-15 09:26:42 +0000

    remove old config

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/user/git/goxgui/g
+# Form implementation generated from reading ui file '/Users/user/git/goxgui/goxgui/ui/preferences.ui'
+#
+# Created: Mon May 27 21:32:10 2013
+#      b
