f134788eca10dc068627206e687246b816025099
Briose <69117706+Briose@users.noreply.github.com>
2021-05-17 11:46:53 +0000

    Edit version

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -12,4 +12,7 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    _fromUtf8 = 
+    _fromUtf8 = lambda s: s
+
+class Ui_Preferences(object):
+    def setupUi
