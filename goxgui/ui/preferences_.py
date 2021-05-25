4b4b775050ca7b801e1f6179724ca0e9f90ac6dc
Briose <69117706+Briose@users.noreply.github.com>
2021-05-25 11:14:52 +0000

    cleaner output

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -40,4 +40,4 @@ class Ui_Preferences(object):
         self.label_6.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_6.setObjectName(_fromUtf8("label_6"))
         self.gridLayout_2.addWidget(self.label_6, 2, 0, 1, 1)
-       
+        self.comboBoxCurrency = QtGui.QComboBox(self.tabCurr
