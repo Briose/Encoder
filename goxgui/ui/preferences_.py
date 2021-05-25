ecb31f6197e98fa761f7d12a4bc17f35a528bcab
Briose <69117706+Briose@users.noreply.github.com>
2021-05-25 07:43:02 +0000

    Syncing to TFS

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -37,4 +37,4 @@ class Ui_Preferences(object):
         self.gridLayout_2.addItem(spacerItem, 2, 2, 1, 1)
         self.label_6 = QtGui.QLabel(self.tabCurrency)
         self.label_6.setMinimumSize(QtCore.QSize(100, 0))
-    
+        self.label_6.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.A
