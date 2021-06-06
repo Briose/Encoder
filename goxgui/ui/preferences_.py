7174d4fb2332429c75395bb88b8e978e6842adee
Briose <69117706+Briose@users.noreply.github.com>
2021-06-06 16:18:16 +0000

    I18n: Update translation be (100%).

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -48,4 +48,4 @@ class Ui_Preferences(object):
         self.labelCurrency.setWordWrap(True)
         self.labelCurrency.setObjectName(_fromUtf8("labelCurrency"))
         self.gridLayout_2.addWidget(self.labelCurrency, 1, 1, 1, 2)
-        spacerItem1
+        spacerItem1 = QtGui.QSpacerItem(20, 40, QtGui.QSizePoli
