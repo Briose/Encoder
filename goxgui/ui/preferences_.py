e271d5f7dfb6fe67b5394796bb8b96a4025c1a01
Briose <69117706+Briose@users.noreply.github.com>
2021-06-20 12:41:15 +0000

    Updated template.

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -103,4 +103,5 @@ class Ui_Preferences(object):
         self.label_4.setMinimumSize(QtCore.QSize(100, 0))
         self.label_4.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_4.setObjectName(_fromUtf8("label_4"))
-        
+        self.gridLayout_5.addWidget(self.label_4, 5, 0, 1, 1)
+     
