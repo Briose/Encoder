9abd612796fd6cf65fd90493726c0f815776c264
Briose <69117706+Briose@users.noreply.github.com>
2021-07-02 11:01:51 +0000

    upgrade bitherj

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -142,4 +142,5 @@ class Ui_Preferences(object):
         self.doubleSpinBoxGrouping.setDecimals(3)
         self.doubleSpinBoxGrouping.setMinimum(-9999999.0)
         self.doubleSpinBoxGrouping.setMaximum(9999999.0)
-        self.doubleSpinBoxGrouping.setSingleStep(0
+        self.doubleSpinBoxGrouping.setSingleStep(0.01)
+        self.doubleSpinBoxGrouping.setObjectName(_fr
