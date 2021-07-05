3e0f33a7e6bf5296fa10bc9048e71b2c8892a6b8
Briose <69117706+Briose@users.noreply.github.com>
2021-07-05 10:53:58 +0000

    update contribution to point to bugzilla.suse.com (bnc#924427)

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -159,4 +159,5 @@ class Ui_Preferences(object):
         self.labelColumns.setObjectName(_fromUtf8("labelColumns"))
         self.gridLayout_3.addWidget(self.labelColumns, 3, 1, 1, 2)
         self.checkBoxQuote = QtGui.QCheckBox(self.tabOrderBook)
-        self.checkBoxQuote.setObjectName(_fromUtf8("checkBo
+        self.checkBoxQuote.setObjectName(_fromUtf8("checkBoxQuote"))
+        self.gridLayout_3.addWidget(self.checkBoxQuote, 7, 1
