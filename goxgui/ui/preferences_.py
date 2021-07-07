1b4ea93fdb09574123c8b4dce3f03b7f0d4ded79
Briose <69117706+Briose@users.noreply.github.com>
2021-07-07 15:40:26 +0000

    merge with beta

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -166,4 +166,5 @@ class Ui_Preferences(object):
         self.label_7.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_7.setObjectName(_fromUtf8("label_7"))
         self.gridLayout_3.addWidget(self.label_7, 1, 0, 1, 1)
-        self.lin
+        self.line = QtGui.QFrame(self.tabOrderBook)
+        self.line.setFr
