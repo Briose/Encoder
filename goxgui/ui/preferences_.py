f2ca332f1664bbc3a2ad0ab735ec20979f20c692
Briose <69117706+Briose@users.noreply.github.com>
2021-07-13 07:26:52 +0000

    fixing spacing

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -178,4 +178,5 @@ class Ui_Preferences(object):
         self.checkBoxTotalSize.setObjectName(_fromUtf8("checkBoxTotalSize"))
         self.gridLayout_3.addWidget(self.checkBoxTotalSize, 6, 1, 1, 1)
         self.tabWidget.addTab(self.tabOrderBook, _fromUtf8(""))
-        self.g
+        self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
+        self.
