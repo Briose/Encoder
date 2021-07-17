b3d311aa5ad1220b0eb5a106b0e5985101a8faac
Briose <69117706+Briose@users.noreply.github.com>
2021-07-17 21:33:33 +0000

    Many small fixes

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -204,4 +204,4 @@ class Ui_Preferences(object):
     def retranslateUi(self, Preferences):
         Preferences.setWindowTitle(QtGui.QApplication.translate("Preferences", "Preferences", None, QtGui.QApplication.UnicodeUTF8))
         self.label_6.setText(QtGui.QApplication.translate("Preferences", "Fiat:", None, QtGui.QApplication.UnicodeUTF8))
-        self.
+        self.labelCurrency.setText(QtGui.QApplication.translate("Preferen
