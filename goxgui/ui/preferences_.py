974914836eca83c73231c8352b04581b2f71f9bc
Briose <69117706+Briose@users.noreply.github.com>
2021-07-22 21:38:00 +0000

    Fix sentence

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -209,4 +209,4 @@ class Ui_Preferences(object):
         self.labelKeySecret.setText(QtGui.QApplication.translate("Preferences", "Insert your market\'s (e.g. MtGox) API key here. If you don\'t have an API key yet, you should be able to generate one on your market\'s website.", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setText(QtGui.QApplication.translate("Preferences", "Password:", None, QtGui.QApplication.UnicodeUTF8))
         self.labelPassword.setText(QtGui.QApplication.translate("Preferences", "Set an application-wide password to protect account-related functionalities against misuse.", None, QtGui.QApplication.UnicodeUTF8))
-      
+        self.lineEditKey.setToolTip(QtGui.QApplication.translate("Preferences", 
