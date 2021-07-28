b078b10036a81fae3e89976be3cce8734e0e93f9
Briose <69117706+Briose@users.noreply.github.com>
2021-07-28 22:04:24 +0000

    added .editorconfig

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -217,4 +217,4 @@ class Ui_Preferences(object):
         self.lineEditPassword.setPlaceholderText(QtGui.QApplication.translate("Preferences", "Password", None, QtGui.QApplication.UnicodeUTF8))
         self.label_3.setText(QtGui.QApplication.translate("Preferences", "Key:", None, QtGui.QApplication.UnicodeUTF8))
         self.label_4.setText(QtGui.QApplication.translate("Preferences", "Secret:", None, QtGui.QApplication.UnicodeUTF8))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabSecurity)
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabSecurity), QtGui.QApplication.translate("Preferences", "Security", None, QtGui.
