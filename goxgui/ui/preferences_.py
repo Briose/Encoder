2d4e1f25d864a824c4873b45106c3b08ff50626e
Briose <69117706+Briose@users.noreply.github.com>
2021-07-30 10:52:55 +0000

    Updated language files from Transifex

--- goxgui/ui/preferences_.py
+++ goxgui/ui/preferences_.py
@@ -219,4 +219,5 @@ class Ui_Preferences(object):
         self.label_4.setText(QtGui.QApplication.translate("Preferences", "Secret:", None, QtGui.QApplication.UnicodeUTF8))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabSecurity), QtGui.QApplication.translate("Preferences", "Security", None, QtGui.QApplication.UnicodeUTF8))
         self.label_5.setText(QtGui.QApplication.translate("Preferences", "Offset:", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_8.setText(QtGui.QApplication.translate("Preferences", "Columns:", None, QtGui.QApplication.
+        self.label_8.setText(QtGui.QApplication.translate("Preferences", "Columns:", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkBoxTotalQuote.setText(QtGui.QAppli
