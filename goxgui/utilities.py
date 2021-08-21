0815c9b8ed8d03faf30a52713e86cad0dac8c8e3
Briose <69117706+Briose@users.noreply.github.com>
2021-08-21 22:37:34 +0000

    bump version to 1.2.2

--- goxgui/utilities.py
+++ goxgui/utilities.py
@@ -89,3 +89,8 @@ def resource_path(relative_path):
         base_path = getattr(sys, '_MEIPASS', os.getcwd())
     except Exception:
         base_path = os.path.abspath('.')
+
+    return os.path.join(base_path, relative_path)
+
+
+def get_
