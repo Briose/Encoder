bfadfd662ad3c7931be5cf9e81abcb6e4c886866
Briose <69117706+Briose@users.noreply.github.com>
2021-08-31 22:06:43 +0000

    post merge fix

--- goxgui/view.py
+++ goxgui/view.py
@@ -51,3 +51,61 @@ class View(QMainWindow):
         self.ui.tableBid.clicked.connect(
             self.slot_update_price_from_bids)
         self.ui.pushButtonCancel.released.connect(
+            self.cancel_order)
+        self.ui.textBrowserStatus.anchorClicked.connect(
+            self.order_selected)
+        self.ui.pushButtonSize.released.connect(
+            self.recalculate_size)
+        self.ui.pushButtonPrice.released.connect(
+            self.update_price_best)
+        self.ui.pushButtonTotal.released.connect(
+            self.recalculate_total)
+        self.ui.actionPreferences_2.triggered.connect(
+            self.show_preferences)
+
+        # associate log channels with their check boxes
+        self.logchannels = [
+            [self.ui.checkBoxLogTicker, 'tick'],
+            [self.ui.checkBoxLogTrade, 'TRADE'],
+            [self.ui.checkBoxLogDepth, 'depth'],
+        ]
+
+        # set correct resizing for the bid and ask tables
+        self.ui.tableAsk.horizontalHeader().setResizeMode(QHeaderView.Stretch)
+        self.ui.tableBid.horizontalHeader().setResizeMode(QHeaderView.Stretch)
+
+        # set up info table
+        self.info = Info(self, self.preferences, self.ui.tableInfo.clicked)
+        self.ui.tableInfo.setModel(self.info)
+        self.ui.tableInfo.horizontalHeader().setResizeMode(QHeaderView.Stretch)
+
+        # connect to signals from info table
+        self.info.signal_base_balance_clicked.connect(
+            self.set_trade_size_from_wallet)
+        self.info.signal_quote_balance_clicked.connect(
+            self.set_trade_total_from_wallet)
+
+        # initializes dynamic ui elements
+        self.init()
+
+        # activate market
+        self.market.start()
+
+        # show main window
+        self.adjustSize()
+        self.show()
+        self.raise_()
+
+    def get_base_currency(self):
+        return self.preferences.get_currency(Preferences.CURRENCY_INDEX_BASE)
+
+    def get_quote_currency(self):
+        return self.preferences.get_currency(Preferences.CURRENCY_INDEX_QUOTE)
+
+    def init(self):
+
+        # initialize wallet values
+        self.info.set_wallet_a(None)
+        self.info.set_wallet_b(None)
+
+        # initialize ticker values
