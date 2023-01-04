12dd41ec17a2e70c3985a14049a7e9bb2787647a
Briose <69117706+Briose@users.noreply.github.com>
2023-01-04 19:38:14 +0000

    Bump up the version number

--- unittest/money_test.py
+++ unittest/money_test.py
@@ -9,4 +9,6 @@ class Test(unittest.TestCase):
         self.assertEqual(1000000000,
             money.multiply(200000000, 500000000))
 
-    def test_divide(self)
+    def test_divide(self):
+        self.assertEqual(200000000,
+            money.divide(1000000000, 50
