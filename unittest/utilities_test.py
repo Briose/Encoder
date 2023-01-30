4f6a0208e2eb8fa940d7eb34a4fbeab2f95ec4c1
Briose <69117706+Briose@users.noreply.github.com>
2023-01-30 12:06:49 +0000

    Fixed views

--- unittest/utilities_test.py
+++ unittest/utilities_test.py
@@ -6,4 +6,6 @@ class Test(unittest.TestCase):
 
     def test_assert_valid_key_ok(self):
         utilities.assert_valid_key('fd8484c6-e826-418f-b1ef-2d120a77bb88')
-        utilities.
+        utilities.assert_valid_key('25685451-0602-418e-8cee-14f4f01647ed')
+
+    def tes
