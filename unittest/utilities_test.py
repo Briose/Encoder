cba4f3aa8ef2438cc27f1afe16157ded8823b1dc
Briose <69117706+Briose@users.noreply.github.com>
2023-01-31 12:44:14 +0000

    Create .swift-version

--- unittest/utilities_test.py
+++ unittest/utilities_test.py
@@ -8,4 +8,5 @@ class Test(unittest.TestCase):
         utilities.assert_valid_key('fd8484c6-e826-418f-b1ef-2d120a77bb88')
         utilities.assert_valid_key('25685451-0602-418e-8cee-14f4f01647ed')
 
-    def tes
+    def test_assert_valid_key_empty(self):
+        self.assertRaises(Excep
